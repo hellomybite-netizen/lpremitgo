617bd7eaf004f20c463120f1bafe4dee274209d7
Rijal <macbook@MacBook-Air-2.lan>
2026-06-27 07:02:52 +0700

    Fix deployment config and set dynamic base path for Docker container

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import fs from 'fs';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+  const isDocker = fs.existsSync('/.dockerenv');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/landing-page-ads/',
+    base: isDocker ? '/' : '/landing-page-ads/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
